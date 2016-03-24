c7f5dd6acfd781904b1f4d3d26c7a33efdc7f78a
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-03-24 17:26:23 -0500

    add method encrypt. It works.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,22 +1,32 @@
 ###define a method called ENCRYPT
 ### go through each character of the string
 ### move to the next letter
-### change each letter into the next one
-def encrypt(string)
+### continue until the last letter of the string
+### change each letter into the next letter of the alphabet
+def encrypt(apple)
+	index = 0
+	while index < apple.length
+		if apple[index] == "z"
+			puts "a"
+		else 
+			puts apple[index].next
+		end
+		index += 1
+	end
+end
 	
-	
-
-
-
-### define  a method called DECRYPT
-###
-def decrypt(string)
+###set the index for each letter in the string
+###take 	
+def decrypt(bananas)
+	bananas.index("abcdefghijklmnopqrstuvwxyz")
+	puts bananas
+end
 
 
 
 
 ### test methods
-encrypt("abc")
-encrypt("zed")
+#encrypt("abc")
+#encrypt("zed")
 decrypt("bcd")
 decrypt("afe")
