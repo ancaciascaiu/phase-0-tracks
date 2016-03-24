609c95c1cbc8768e0266313aee030935a94bca36
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-03-24 14:20:24 -0500

    add pseudocode and frame

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,22 @@
+###define a method called ENCRYPT
+### go through each character of the string
+### move to the next letter
+### change each letter into the next one
+def encrypt(string)
+	
+	
+
+
+
+### define  a method called DECRYPT
+###
+def decrypt(string)
+
+
+
+
+### test methods
+encrypt("abc")
+encrypt("zed")
+decrypt("bcd")
+decrypt("afe")
