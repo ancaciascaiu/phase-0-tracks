fc8728d2e6c5ab7bd4ce53bb3964fd73c0d4337e
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-03-29 16:37:57 -0500

    add hash and print it

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,5 +1,9 @@
 #create form questions
-#
+#create an empty hash
+#assign new symbols(keys) to the given variable names(values)
+
+puts "Enter the following info for your client"
+
 puts "What`s the client`s name?"
 name = gets.chomp
 
@@ -13,5 +17,16 @@ puts "Decor theme"
 decor_theme = gets.chomp
 
 puts "Do they have pets? true/false"
-pets = gets.chomp.to_b
+pets = gets.chomp
+if pets == "yes"
+	pets = true
+elsif pets == "no"
+	pets = false
+end
+
+client1 = Hash.new
+client1 = {name: name, age: age, children: number_of_children, decor: decor_theme, pets: pets}
+
+print "Our database has the following entries: ", client1
+
 
