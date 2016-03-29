5c8a8a84c85cafe6ddddd30eed576f8d3fa5b268
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-03-29 15:57:44 -0500

    create questions

---
+++ ruby/hashes.rb
@@ -0,0 +1,17 @@
+#create form questions
+#
+puts "What`s the client`s name?"
+name = gets.chomp
+
+puts "How old is the client?"
+age = gets.chomp.to_i
+
+puts "Number of children"
+number_of_children = gets.chomp.to_i
+
+puts "Decor theme"
+decor_theme = gets.chomp
+
+puts "Do they have pets? true/false"
+pets = gets.chomp.to_b
+
