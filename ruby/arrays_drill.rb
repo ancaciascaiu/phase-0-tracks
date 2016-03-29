aea716960f02d8902f2de70f3d353109f1662986
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-03-29 12:21:58 -0500

    add items, detele & insert items to the array

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,15 @@
+aquarium = []
+p aquarium
+
+aquarium << "clown fish"
+aquarium.insert(1, "coral", "anemone", "crab", "shrimp")
+p aquarium
+
+aquarium .delete_at(2)
+p aquarium
+
+aquarium.insert(2, "rocks")
+p aquarium
+
+
+
