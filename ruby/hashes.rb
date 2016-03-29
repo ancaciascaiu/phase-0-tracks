8abc5570f9cd51a97a40ec95f49a1ac4abbd9f27
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-03-29 17:18:02 -0500

    add the database update

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -16,7 +16,7 @@ number_of_children = gets.chomp.to_i
 puts "Decor theme"
 decor_theme = gets.chomp
 
-puts "Do they have pets? true/false"
+puts "Do they have pets? yes/no"
 pets = gets.chomp
 if pets == "yes"
 	pets = true
@@ -27,6 +27,22 @@ end
 client1 = Hash.new
 client1 = {name: name, age: age, children: number_of_children, decor: decor_theme, pets: pets}
 
-print "Our database has the following entries: ", client1
+puts "Our database has the following entries: ", client1
+print
+puts "Any keys you wanna update? (none if you don`t)"
+key = gets.chomp.to_sym
+puts "What`s your new entry for this key?"
+value = gets.chomp
+if key == "none"
+	exit 
+else
+	client_update = {key => value}
+	client1.merge!(client_update)
+end
+
+print "This is the changed database: ", client1
+
+#yesssss....i did it!
+# :D
 
 
