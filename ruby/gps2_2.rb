400f476c919d6dd01e520b540170e66a3c88550f
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-04-05 17:24:40 -0500

    give answers

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,18 @@
+What did you learn about pseudocode from working on this challenge?
+it is very important to write it before you start writing the code. It gets to be much faster and clearer.
+
+What are the tradeoffs of using arrays and hashes for this challenge?
+arrays and hashes are very different. You can only use one or the other.
+
+What does a method return?
+it returns whatever you set it to return.
+
+What kind of things can you pass into methods as arguments?
+whatever you want to take into consideration as input, for the method to compute stuff using it.
+
+How can you pass information between methods?
+calling a method inside the other or naming global variables.
+
+What concepts were solidified in this challenge, and what concepts are still confusing?
+i think i m one step further in understanding how to build and code a method, and i don`t know what`s confusing... 
+it`s like you don`t know what else can happen so you don`t know how to utter questions. :))
