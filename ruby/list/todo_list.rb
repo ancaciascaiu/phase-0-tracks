df51a88d7e342866e4936182711ba0d58909218a
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-04-08 18:09:11 -0500

    add the rest of the methods

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,6 +1,22 @@
 class TodoList
-	def get_items(a, b)
-		[a, b]
+	def initialize(things)
+		@things = things
+	end
+	
+	def get_items
+		@things
+	end
+
+	def add_item(item)
+		@things << item
+	end
+
+	def delete_item(item)
+		@things.delete(item)
+	end
+
+	def get_item(ind)
+		@things[ind]
 	end
 end
 
