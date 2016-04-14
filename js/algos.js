d436bdf587a213b18a055f957607436371d23135
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-04-13 23:52:14 -0500

    exit

--- js/algos.js
+++ js/algos.js
@@ -1,23 +1,36 @@
 //write a function that takes an array of words or phrases 
 	//and returns the longest word in the array.
 
-ary = ["long phrase","longest phrase","longer phrase"]
 
-//make a loop
-for (i = 0; i < ary.length-1; i++){
-	//get the length of the current element in the array
-	strlgth = ary[i].length;
-	//get the length of the next element in the array
-	nextlgth = ary[i+1].length;
+function theEst(ary){
 	
-	//initialize the first string in the array as the longest so far
-	longest = ary[0];
-	//compare each string with the next one in the array, return the longest
-	if (strlgth > nextlgth) {
-		longest = strlgth;
-	} else{
-		longest = nextlgth;
+	//initialize the longest string so far
+	longeststr = ary[0];
+	
+	//make a loop
+	for (i = 0; i < ary.length-1; i++){
+		//get the current element in the array
+		currstr = longeststr;
+		
+		//get the next element in the array
+		nextstr = ary[i+1];
+		
+		//compare each string with the next one in the array, return the longest
+		if (currstr.length > nextstr.length) {
+			longeststr = currstr;
+			
+
+		} else {
+			longeststr = nextstr;
+			
+		}
+		
 	}
+
+	console.log (longeststr, longeststr.length);
 }
 
-console.log(longest)
+theEst(["long phrase","longest phrase","longer phrase"]);
+theEst(["street", "town", "city", "whole wide world", "country", "continent"]);
+theEst(["panther", "crocodile", "ox", "lion", "worms", "pig"]);
+
